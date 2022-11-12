a2cf7f2a4fcf6151d6afd1fb67abaaeb0a2e8a16
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2022-11-12 16:02:56 -0300

    Feat - Created initial configs

---
+++ lib/flight_booking_report.ex
@@ -0,0 +1,18 @@
+defmodule FlightBookingReport do
+  @moduledoc """
+  Documentation for `FlightBookingReport`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FlightBookingReport.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/flight_booking_report_test.exs
@@ -0,0 +1,8 @@
+defmodule FlightBookingReportTest do
+  use ExUnit.Case
+  doctest FlightBookingReport
+
+  test "greets the world" do
+    assert FlightBookingReport.hello() == :world
+  end
+end
