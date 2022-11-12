096fe5163b463f47696ebdd325c0eee172356b5e
Eduardo Andrade <eduardo.andrade1111@gmail.com>
2022-11-12 18:10:18 -0300

    created report file

---
+++ lib/report/Report.ex
@@ -0,0 +1,27 @@
+defmodule FlightBookingReport.Report.Report do
+  alias FlightBookingReport.Bookings.Agent, as: BookingAgent
+  alias FlightBookingReport.Bookings.Booking
+
+  def create(initial_date, final_date, filename \\ "report.csv") do
+    path = "reports/#{filename}"
+
+    booking_list = build_booking_list(initial_date, final_date)
+
+    File.write!(path, booking_list)
+  end
+
+  defp build_booking_list(initial_date, final_date) do
+    BookingAgent.get_all()
+    |> Map.values()
+    |> Enum.filter(&filter_booking(&1, initial_date, final_date))
+    |> Enum.map(&booking_string/1)
+  end
+
+  defp filter_booking(%Booking{complete_date: complete_date}, initial_date, final_date) do
+    complete_date >= initial_date && complete_date <= final_date
+  end
+
+  defp booking_string(%Booking{} = booking) do
+    "#{booking.user_id},#{booking.local_origin},#{booking.local_destination},#{booking.complete_date}"
+  end
+end
